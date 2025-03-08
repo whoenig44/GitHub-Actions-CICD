b0bc19c0bc2b54d6fde653389fabba91dd4fbc3a
whoenig44 <will.hoenig44@gmail.com>
2025-03-07 20:03:00 -0600

    Fixed code so it runs

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,8 +1,15 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pythonQuestionsPath = path.resolve(__dirname, './pythonQuestions.json');
+const pythonQuestions = JSON.parse(fs.readFileSync(pythonQuestionsPath, 'utf-8'));
 
 db.once('open', async () => {
   await cleanDB('Question', 'questions');

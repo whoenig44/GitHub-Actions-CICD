3e0ef30c0f9c80599ea6ce70931b34da0df2f336
whoenig44 <will.hoenig44@gmail.com>
2025-03-09 07:59:02 -0500

    Fixed production issues

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,9 +3,12 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
+    if (!models[modelName] || !models[modelName].db) {
+      throw new Error(`Model or database not found for modelName: ${modelName}`);
+    }
     let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
-    }).toArray()
+    }).toArray();
 
     if (modelExists.length) {
       await db.dropCollection(collectionName);
